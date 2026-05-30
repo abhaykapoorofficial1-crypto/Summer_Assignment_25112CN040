0f77b8b0a0b1c3517d8713aba09c8ed7be9c76b2
abhaykapoorofficial1-crypto <abhaykapoorofficial1@gmail.com>
2026-05-30 10:57:03 +0530

    Add files via upload

---
+++ Q9.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = input.nextInt();
+        int count = 0;
+        for(int i = 1 ; i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.println("Prime Number");
+        }
+        else{
+            System.out.println("Not Prime Number");
+        }
+    }
+
+    
+}
