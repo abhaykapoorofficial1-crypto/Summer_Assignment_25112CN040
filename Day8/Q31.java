1db02f087fdfb21b9ed9a20994a2cd21831406d8
abhaykapoorofficial1-crypto <abhaykapoorofficial1@gmail.com>
2026-06-04 14:07:05 +0530

    Add files via upload

---
+++ Day8/Q31.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q31 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int n = input.nextInt();
+        char ch = 'A';
+        for(int i =0 ; i<=n ; i++){
+            for(int j =0 ; j<=i ; j++){
+                System.out.print((char)(ch + j));
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day8/Q32.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Q32 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int n  = input.nextInt();
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
