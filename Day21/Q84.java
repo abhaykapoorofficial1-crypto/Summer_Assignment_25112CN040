da153b5f2d80377e92591c30c677727fac2b3692
abhaykapoorofficial1-crypto <abhaykapoorofficial1@gmail.com>
2026-06-17 12:12:07 +0530

    Add files via upload

---
+++ Day21/Q84.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Q84 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter String");
+        String str = input.nextLine();
+        String upper = str.toUpperCase();System.out.println("Uppercase String " + upper);
+    }
+}
