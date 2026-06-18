2b1865c371814ea865afa7517e12788641a208b7
abhaykapoorofficial1-crypto <abhaykapoorofficial1@gmail.com>
2026-06-18 10:08:13 +0530

    Create Q85.java

---
+++ Day22/Q85.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Q85 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter the string");
+        String str = input.nextLine();
+        String rev ="";
+        for(int i = str.length() - 1 ; i>=0 ; i--){
+            rev += str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
